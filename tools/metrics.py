5ed191b93ac32ae1ed3a151bf576e59478176841
sairam-reverie <c.sandeep@reverieinc.com>
2022-12-12 14:15:21 +0530

    Change torchmetrics to sklearn metrics

--- tools/metrics.py
+++ tools/metrics.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
 import torch
+from sklearn.metrics import confusion_matrix,accuracy_score,f1_score
+
 from torchmetrics.classification import BinaryAccuracy,BinaryF1Score
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
@@ -9,24 +11,31 @@ f1_score_fn = BinaryF1Score().to(device)
 
 weight = torch.Tensor([0.1,0.9]).to(device)
 
-def get_preds(scores,threshold=0.5):
-    probs = F.softmax(scores,dim=-1)
-    pos_probs = probs[:,-1]
-    preds = (pos_probs>threshold).long()
-    return preds
-
 def get_loss(scores,labels,weight=None):
     return F.cross_entropy(scores,labels,weight)
 
+def get_preds(scores,threshold=0.5):
+    probs = F.softmax(scores,dim=-1)
+    pos_probs = probs[:,-1]
+    preds = (pos_probs>threshold).long().tolist()
+    return preds
+
 def get_accuracy(preds,labels):
-    return accuracy_fn(preds,labels)
+    return accuracy_score(labels.tolist(),preds, normalize=True)
+    #return accuracy_fn(preds,labels)
 
 def get_f1_score(preds,labels):
-    return f1_score_fn(preds,labels)
+    return f1_score(labels.tolist(),preds)
+    #return f1_score_fn(preds,labels)
+def get_confusion_matrix(preds,labels):
+    conf_matrix = confusion_matrix(labels.tolist(),preds)#*, labels=None, sample_weight=None, normalize=None
+    tn, fp, fn, tp = conf_matrix.ravel()
+    return (tn, fp, fn, tp)
 
 def get_metrics(scores,labels):
     preds = get_preds(scores)
     loss = get_loss(scores,labels)
     accuracy = get_accuracy(preds,labels)
     f1_score = get_f1_score(preds,labels)
-    return {"loss":loss,"accuracy":accuracy,"f1_score":f1_score}
+    tn, fp, fn, tp = get_confusion_matrix(preds,labels)
+    return {"loss":loss,"accuracy":accuracy,"f1_score":f1_score,"tn":tn,"fp":fp,"fn":fn,"tp":tp}
