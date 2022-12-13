3d07b817c8eba8b4444d3849d56467bb6bfb0a93
sairam-reverie <c.sandeep@reverieinc.com>
2022-12-13 08:47:56 +0530

    Add balanced sampling feature

--- tools/data_utils.py
+++ tools/data_utils.py
@@ -3,6 +3,8 @@ import numpy as np
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
 from torch.utils.data import DataLoader, Dataset
 from pathlib import Path
+from torchsampler import ImbalancedDatasetSampler
+
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
@@ -29,6 +31,9 @@ class AudioClassification_Dataset(Dataset):
         label = int(name[0])
         return (array,label,name)
 
+def get_label(dataset):
+    return [example[1] for example in dataset]
+
 def pad_collate(batch):
     samples,labels,names = zip(*batch)
     lengths = [sample.shape[0] for sample in samples]
@@ -37,9 +42,13 @@ def pad_collate(batch):
     lengths = torch.tensor(lengths).to(device)
     return source_padded,labels,lengths,names
 
-def get_data_loader(data_dir,batch_size):
+def get_data_loader(data_dir,batch_size,balanced=False):
     dataset = AudioClassification_Dataset(data_dir)
-    dataloader = DataLoader(dataset, batch_size = batch_size, collate_fn=pad_collate)
+    if balanced:
+        sampler=ImbalancedDatasetSampler(dataset,callback_get_label=get_label)
+        dataloader = DataLoader(dataset, batch_size = batch_size, collate_fn=pad_collate,sampler=sampler)
+    else:
+        dataloader = DataLoader(dataset, batch_size = batch_size, collate_fn=pad_collate)
     return dataloader
 
 
@@ -52,16 +61,29 @@ if __name__ == "__main__":
     trn_array_dir = wake_word_array_dir / "trn_set"
     val_array_dir = wake_word_array_dir / "val_set"
 
-    batch_size = 4
+    batch_size = 16
     val_set = AudioClassification_Dataset(val_array_dir)
-    val_loader = DataLoader(val_set, batch_size = batch_size, collate_fn=pad_collate)
-    batch = next(iter(val_loader))
-    samples,labels,lengths,names = batch
-    print(samples.shape)
-    labels = labels.tolist()
-    lengths = lengths.tolist()
-    for bunch in zip(labels,lengths,names):
-        print(bunch)
+    val_len = len(val_set)
+    print(val_len)
+    batch_size=10
+    val_loader = get_data_loader(val_array_dir,batch_size,balanced=True)
+    num_iterations = (val_len*2)//batch_size
+    val_iter = iter(val_loader)
+    for _ in range(num_iterations):
+        batch = next(val_iter)
+        print(batch[1])
+    # #val_loader = DataLoader(val_set, batch_size = batch_size, collate_fn=pad_collate)
+    # #labels = 
+    # sampler=ImbalancedDatasetSampler(val_set,callback_get_label=get_label)
+    # val_loader = DataLoader(dataset=val_set,sampler=sampler,batch_size=batch_size,collate_fn=pad_collate)
+    # batch = next(iter(val_loader))
+    # samples,labels,lengths,names = batch
+    # print(samples.shape)
+    # for i
+    # labels = labels.tolist()
+    # lengths = lengths.tolist()
+    # for bunch in zip(labels,lengths,names):
+    #     print(bunch)
 
 
     # array,label = val_set[0]
